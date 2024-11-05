5c0a6e20fc00c33589630112d4b7dbe4e3454aa4
Jahmora <Miphonej13@gmail.com>
2024-11-05 23:34:06 +0100

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,15 @@
+const translations = {
+    fr: {
+      purchaseTitle: "Options d'Achat",
+      description: "Achetez cette image avec des crypto-monnaies.",
+      buyButton: "Achetez Maintenant",
+      errorMessage: "Une erreur est survenue lors du chargement de l'image."
+    },
+    en: {
+      purchaseTitle: "Purchase Options",
+      description: "Buy this image with cryptocurrency.",
+      buyButton: "Buy Now",
+      errorMessage: "An error occurred while loading the image."
+    }
+  };
+  
