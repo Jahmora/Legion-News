f9dd6cc4223e14d4971dc11214b13d9bbc994e1f
Jahmora <Miphonej13@gmail.com>
2024-11-10 14:04:23 +0100

    Update translations.js

--- translations.js
+++ translations.js
@@ -1,15 +1,20 @@
 const translations = {
-    fr: {
-      purchaseTitle: "Options d'Achat",
-      description: "Achetez cette image avec des crypto-monnaies.",
-      buyButton: "Achetez Maintenant",
-      errorMessage: "Une erreur est survenue lors du chargement de l'image."
-    },
-    en: {
-      purchaseTitle: "Purchase Options",
-      description: "Buy this image with cryptocurrency.",
-      buyButton: "Buy Now",
-      errorMessage: "An error occurred while loading the image."
-    }
-  };
+  fr: {
+    pageTitle: "Legion News Interactive",
+    purchaseOptionsTitle: "Options d'Achat",
+    purchaseDescription: "Achetez cette image avec des crypto-monnaies.",
+    buyButton: "Achetez Maintenant",
+    languageLabel: "Langue :",
+    errorMessage: "Erreur de chargement de l'image."
+  },
+  en: {
+    pageTitle: "Legion News Interactive",
+    purchaseOptionsTitle: "Purchase Options",
+    purchaseDescription: "Buy this image with cryptocurrencies.",
+    buyButton: "Buy Now",
+    languageLabel: "Language:",
+    errorMessage: "Error loading image."
+  }
+};
+
   
